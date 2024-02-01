fb80f34f55225720b3662a61bd37c97ad0fde063
MuhdAdil13 <adilrahim333@gmail.com>
2024-02-01 22:01:29 +0530

    typing anim exported to this file

---
+++ script.js
@@ -0,0 +1,10 @@
+//Text typing Animation
+
+let typingEffect= new Typed(".name-spc",{
+    strings:["MUHAMMED ADIL","FULL STACK DEVELOPER"],
+    loop:true,
+    typeSpeed:80,
+    backSpeed:50,
+    startDelay:500,
+    backDelay:2500
+})
