7ac768166425e3bd164e45a921651865bfe5792c
MuhdAdil13 <adilrahim333@gmail.com>
2024-02-04 22:12:26 +0530

    new animation added

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ let typingEffect= new Typed(".name-spc",{
     startDelay:500,
     backDelay:2500
 })
+
+// For Smooth Scrolling
+
+// const navLinks = document.querySelectorAll('.mylink');
+
+// navLinks.forEach(link => {
+//   link.addEventListener('click', function(event) {
+//     event.preventDefault(); // Prevent default anchor link behavior
+
+//     const targetSection = document.getElementById(this.hash.substring(1));
+
+//     // Enhanced smooth scrolling with optional offset for navbar height
+//     window.scrollTo({
+//       top: targetSection.offsetTop - (nav.offsetHeight || 50), // Adjust offset if needed
+//       behavior: 'smooth'
+//     });
+//   });
+// });
