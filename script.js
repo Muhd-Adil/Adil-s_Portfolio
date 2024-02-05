c3db0ee3f2fc4d11d45a288dcc72578ba64a864d
MuhdAdil13 <adilrahim333@gmail.com>
2024-02-05 13:01:55 +0530

    footer changed

--- script.js
+++ script.js
@@ -9,20 +9,28 @@ let typingEffect= new Typed(".name-spc",{
     backDelay:2500
 })
 
-// For Smooth Scrolling
+//For Smooth Scrolling
 
-// const navLinks = document.querySelectorAll('.mylink');
+const navLinks = document.querySelectorAll('.mylink');
+const nav = document.querySelector('nav');
+navLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default anchor link behavior
+    console.log(this.hash.substring(1));
+    const targetSection = document.getElementById(this.hash.substring(1));
 
-// navLinks.forEach(link => {
-//   link.addEventListener('click', function(event) {
-//     event.preventDefault(); // Prevent default anchor link behavior
-
-//     const targetSection = document.getElementById(this.hash.substring(1));
-
-//     // Enhanced smooth scrolling with optional offset for navbar height
-//     window.scrollTo({
-//       top: targetSection.offsetTop - (nav.offsetHeight || 50), // Adjust offset if needed
-//       behavior: 'smooth'
-//     });
-//   });
-// });
+    // Enhanced smooth scrolling with optional offset for navbar height
+    if (window.innerWidth <= 768) { // Adjust the width as needed for your mobile breakpoint
+        window.scrollTo({
+          top: targetSection.offsetTop - 80,
+          behavior: 'smooth'
+        });
+      } else {
+        window.scrollTo({
+          top: targetSection.offsetTop - (nav.offsetHeight || 50),
+          behavior: 'smooth'
+        });
+      }
+    console.log(100);
+  });
+});
